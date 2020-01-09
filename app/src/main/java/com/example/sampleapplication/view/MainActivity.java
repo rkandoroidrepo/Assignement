daeeb8fd99aac07680d8c63c16b97cc9718bbaf5
ramkrishnak <ramkrishna.kushwaha@jdplc.com>
2020-01-09 21:01:53 +0530

    ListFragment: fragment added fragment_list.xml: layout added MainActivity: ListFragment invoked in onCreate

--- app/src/main/java/com/example/sampleapplication/view/MainActivity.java
+++ app/src/main/java/com/example/sampleapplication/view/MainActivity.java
@@ -12,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            ListFragment listFragment = ListFragment.newInstance();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, listFragment)
+                    .commit();
+        }
     }
 }
